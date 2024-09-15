416e8b20946381d8d5d479ac924d780f1a6a17f5
Riley Chang <me@rileycki.com>
2024-09-15 11:09:17 +0800

    reuse the process of testing

--- test.js
+++ test.js
@@ -7,10 +7,11 @@ import { h } from 'hastscript';
 import rehypeParse from 'rehype-parse';
 import rehypeStringify from 'rehype-stringify';
 
+import { t, map, flatMap, stopRecursionClassName } from './lib/utils.js';
+
 import { rehypeMarkCompressable } from './lib/compressable.js';
 import { rehypeMarkHangable } from './lib/hangable.js';
 import { rehypeMarkSeparator } from './lib/separator.js';
-import { t, map, flatMap, stopRecursionClassName } from './lib/utils.js';
 
 const openingCompressableClassName = 'r-typography-opening-compressable';
 const closingCompressableClassName = 'r-typography-closing-compressable';
@@ -65,13 +66,23 @@ describe('plugins', function () {
 	const text =
 		'<html><head></head><body><p>《狐狸與獵犬》（英語：The Fox and the Hound）是 Daniel P. Mannix 於 1967 年創作的小說。</p></body></html>';
 
-	test('rehypeMarkCompressable', async function () {
-		const result = await unified()
+	/**
+	 * @import { PluggableList } from 'unified'
+	 */
+
+	/**
+	 * @param {PluggableList} plugin
+	 */
+	const transformText = (plugin) =>
+		unified()
 			.use(rehypeParse)
-			.use(rehypeMarkCompressable)
+			.use(plugin)
 			.use(rehypeStringify)
 			.process(text)
 			.then((vFile) => vFile.value);
+
+	test('rehypeMarkCompressable', async function () {
+		const result = await transformText(rehypeMarkCompressable);
 
 		const openingBookmark = `<span class="${openingCompressableClassName} ${stopRecursionClassName}">《</span>`;
 		const closingBookmark = `<span class="${closingCompressableClassName} ${stopRecursionClassName}">》</span>`;
@@ -84,12 +95,7 @@ describe('plugins', function () {
 	});
 
 	test('rehypeMarkHangable', async function () {
-		const result = await unified()
-			.use(rehypeParse)
-			.use(rehypeMarkHangable)
-			.use(rehypeStringify)
-			.process(text)
-			.then((vFile) => vFile.value);
+		const result = await transformText(rehypeMarkHangable);
 
 		const stopHangableHtml = `<span class="${hangableContainerClassName}"><span class="${hangableClassName} ${stopRecursionClassName}">。</span></span>`;
 		const expectedResult = `<html><head></head><body><p>《狐狸與獵犬》（英語：The Fox and the Hound）是 Daniel P. Mannix 於 1967 年創作的小說${stopHangableHtml}</p></body></html>`;
@@ -98,12 +104,7 @@ describe('plugins', function () {
 	});
 
 	test('rehypeMarkSeparator', async function () {
-		const result = await unified()
-			.use(rehypeParse)
-			.use(rehypeMarkSeparator)
-			.use(rehypeStringify)
-			.process(text)
-			.then((vFile) => vFile.value);
+		const result = await transformText(rehypeMarkSeparator);
 
 		const separatorHtml = `<span class="${separatorClassName}"> </span>`;
 		const expectedResult = `<html><head></head><body><p>《狐狸與獵犬》（英語：The Fox and the Hound）是${separatorHtml}Daniel P. Mannix${separatorHtml}於${separatorHtml}1967${separatorHtml}年創作的小說。</p></body></html>`;
